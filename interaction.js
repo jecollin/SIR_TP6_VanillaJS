214882f4fbe5518815eed89053684111fc32c357
Jean-Baptiste <jean-baptiste.collin@etudiant.univ-rennes1.fr>
2023-04-03 15:15:25 +0200

    Q2

--- interaction.js
+++ interaction.js
@@ -7,9 +7,36 @@ function DnD(canvas, interactor) {
     this.startY = 0;
     this.endX = 0;
     this.endY = 0;
+
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
   }
 
-	// Developper les 3 fonctions gérant les événements
+  function onMouseDown(evt) {
+    this.startX = evt.x;
+    this.startY = evt.y;
+  }
+
+  // Developper les 3 fonctions gérant les événements
+
+
+  function onMouseMove(evt) {
+    this.endX = evt.x;
+    this.endY = evt.y;
+  }
+
+  function onMouseUp(evt) {
+    this.endX = evt.x;
+    this.endY = evt.y;
+
+    // Calculer le déplacement
+    const deltaX = this.endX - this.startX;
+    const deltaY = this.endY - this.startY;
+    console.log(`Le déplacement est de (${deltaX}, ${deltaY})`);
+  }
+
+
 
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
