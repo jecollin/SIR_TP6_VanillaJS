e872dfb702203546becacdd22ce657414998fbc4
Jean-Baptiste <jean-baptiste.collin@etudiant.univ-rennes1.fr>
2023-04-03 15:51:52 +0200

    Test de model.js dans main.js

--- main.js
+++ main.js
@@ -12,11 +12,26 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
 /////
 
 // Code temporaire pour tester l'affiche de la vue
-//var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
-//rec.paint(ctx);
-//var ligne = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
-//ligne.paint(ctx);
+var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+rec.paint(ctx);
+var ligne = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+ligne.paint(ctx);
 // tester également Dessin.
+
+// Création d'un nouveau dessin
+const drawing = new Drawing();
+
+// Ajout d'un rectangle rouge de largeur 100 et hauteur 50, en position (10, 10)
+const rect1 = new Rectangle(10, 10, 100, 50, "red", 1);
+drawing.addShape(rect1);
+
+// Ajout d'une ligne bleue allant de (50, 100) à (150, 200)
+const line1 = new Line(50, 100, 150, 200, "blue", 2);
+drawing.addShape(line1);
+
+// Affichage du dessin dans la console
+console.log(drawing);
+
 ////
 
 // Code final à utiliser pour manipuler Pencil.
